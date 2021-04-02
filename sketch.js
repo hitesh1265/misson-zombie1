29459e15eff11033f551c1575ef6418403b1fc48
hitesh <80262039+hitesh1265@users.noreply.github.com>
2021-04-02 18:30:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -78,7 +78,7 @@ function draw() {
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
     // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-  matter.Body.setStatic(packageBody,false);
+  Matter.Body.setStatic(packageBody,false);
     
   }
 }
